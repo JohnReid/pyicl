f26e0034266dede58da1531cdd38a1e3036403e4
JohnReid <johnbaronreid@netscape.net>
2013-09-25 08:18:47 +0100

    Initial commit

---
+++ siteconf.py
@@ -0,0 +1,6 @@
+BOOST_INC_DIR = ['/home/john/include']
+BOOST_LIB_DIR = ['/home/john/lib']
+BOOST_COMPILER = 'gcc46'
+BOOST_PYTHON_LIBNAME = ['boost_python']
+CXXFLAGS = ['-Wno-sign-compare']
+LDFLAGS = []
